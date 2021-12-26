12200c5d4b9fa6a62481b46388d3fd22cd6d9aae
ccoeurderoy <charles.coeurderoy@algoan.com>
2021-12-26 13:51:59 +0100

    test: improve e2e tests (basic scenarios)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { buildFakeApp } from './utils/app';
+import * as nock from 'nock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     app = await buildFakeApp();
+  });
+
+  afterAll(async () => {
+    nock.restore();
+    await app.close();
   });
 
   it('/ (GET)', () => request(app.getHttpServer()).get('/ping').expect(204).expect({}));
